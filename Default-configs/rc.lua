daf2266d5fa72cb52a3427361e0da6209a464ab6
astronomx <unkownusernotfound@protonmail.com>
2022-12-22 09:51:20 +0100

    UPDATE: New wallpapers

--- Default-configs/rc.lua
+++ Default-configs/rc.lua
@@ -65,19 +65,7 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    awful.layout.suit.tile
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
@@ -173,7 +161,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -314,10 +302,6 @@ globalkeys = gears.table.join(
               end,
               {description = "restore minimized", group = "client"}),
 
-    -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
-
     awful.key({ modkey }, "x",
               function ()
                   awful.prompt.run {
@@ -329,14 +313,39 @@ globalkeys = gears.table.join(
               end,
               {description = "lua execute prompt", group = "awesome"}),
 
+    --- custom functions ---
+    --- ++++++++++++++++
+    --- + applications + 
+    --- ++++++++++++++++
+          
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"}),
-    
+    awful.key({ modkey },   "p", function() menubar.show() end,
+        {description = "show the menubar", group = "launcher"}),
+              
     -- Brave
-    awful.key({ modkey },           "b",    function ()
+    awful.key({ modkey },   "b",    function ()
         awful.util.spawn("brave") end,
-                    {description="opens brave browser", group="applications"})          
+                {description= "open brave browser", group = "applications"}),
+              
+    -- vscode
+    awful.key({ modkey },   "r",    function () 
+        awful.util.spawn("code") end,
+                {description = "open vscode", group = "applications"})
+                
+    --- ++++++++++++++++
+    --- +    system    + 
+    --- ++++++++++++++++
+             
+    --volume
+        -- awful.key({ }, "#122", function () awful.util.spawn("amixer -D pulse sset Master 5%-") end),
+            -- awful.key({ }, "#123", function () awful.util.spawn("amixer -D pulse sset Master 5%+") end),
+                -- awful.key({ }, "#121", function () volume("mute") end),
+             
+    --brightness
+         -- awful.key({ }, "#232", function () awful.util.spawn("xbacklight -dec 10") end),
+            -- awful.key({ }, "#233", function () awful.util.spawn("xbacklight -inc 10") end)
+
+
 )
 
 clientkeys = gears.table.join(
